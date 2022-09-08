cafcc677d49e58328d153dce1fe8f2c71e4b04d7
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-09-08 00:31:02 -0400

    Initial commit

---
+++ adafruit_ble_beacon.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2022 Alec Delaney for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`adafruit_ble_beacon`
+================================================================================
+
+BLE Location Beacon for CircuitPython
+
+
+* Author(s): Alec Delaney
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+import _bleio
+from adafruit_ble.
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/tekktrik/Adafruit_CircuitPython_BLE_Beacon.git"
