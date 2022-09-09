db2395cb668d4deb3593c095de25851dc687c653
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-09-09 13:32:12 -0400

    Get documentation to build

--- adafruit_ble_beacon.py
+++ adafruit_ble_beacon.py
@@ -15,21 +15,11 @@ BLE Location Beacon for CircuitPython
 Implementation Notes
 --------------------
 
-**Hardware:**
-
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
-
 **Software and Dependencies:**
 
 * Adafruit CircuitPython firmware for the supported boards:
   https://circuitpython.org/downloads
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
-
-# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
 import struct
--- docs/examples.rst
+++ docs/examples.rst
@@ -1,8 +1,12 @@
-Simple test
-------------
+Sending and Receiving
+---------------------
 
 Ensure your device works with this simple test.
 
 .. literalinclude:: ../examples/ble_beacon_simpletest.py
-    :caption: examples/ble_beacon_simpletest.py
+    :caption: Receive iBeacon advertisements
+    :linenos:
+
+.. literalinclude:: ../examples/ble_beacon_sendtest.py
+    :caption: Send iBeacon advertisements
     :linenos:
